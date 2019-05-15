83c79d53e62b6d08fef52ce74badd4301c258db5
Thomas Erik Olsen <tolsen@southern.edu>
2019-05-14 20:11:01 -0400

    incomplete algorithm, but progress

--- Part 1 - 1.2 Integer Multiplication/Grade-School Algorithm/multiplyGSA.cpp
+++ Part 1 - 1.2 Integer Multiplication/Grade-School Algorithm/multiplyGSA.cpp
@@ -6,23 +6,36 @@
 
 using namespace std;
 
-int MultiplyGradeSchoolAlgorithm(int x, int y)
-{
-    vector<int> yDigits;
-    CollectDigits(yDigits, y);
-    int place = 1;
-
-    while (!yDigits.empty)
-    {
-        //do stuff - have a meeting to get to.
-    }
-}
-
 void CollectDigits(vector<int>& digits, long long num) {
     if (num > 9) {
         CollectDigits(digits, num / 10);
     }
     digits.push_back(num % 10);
+}
+
+int MultiplyGradeSchoolAlgorithm(int x, int y)
+{
+    vector<int> yDigits;
+    CollectDigits(yDigits, y);
+    int place = 0;
+    long long yCurr;
+    long long retVal = 0;
+
+    while (!yDigits.empty())
+    {
+        yCurr = yDigits.back();
+        yDigits.pop_back();
+        if (place > 0)
+        {
+            retVal += x * yCurr * (pow(10, place));
+        }
+        else
+        {
+            retVal += x & yCurr;
+        }
+    }
+
+    return retVal;
 }
 
 int main(int argc, char *argv[]) {
@@ -40,10 +53,17 @@ int main(int argc, char *argv[]) {
 
     auto t1 = chrono::high_resolution_clock::now();
     int result = x * y;
-    auto t2 = std::chrono::high_resolution_clock::now();
-    std::cout << "Base C++ multiplication took "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count()
-              << " milliseconds\n";
+    auto t2 = chrono::high_resolution_clock::now();
+    cout << "Base C++ multiplication took "
+              << chrono::duration_cast<chrono::milliseconds>(t2-t1).count()
+              << " milliseconds to return " << result << "\n";
+
+    t1 = chrono::high_resolution_clock::now();
+    result = MultiplyGradeSchoolAlgorithm(x, y);
+    t2 = chrono::high_resolution_clock::now();
+    cout << "Grade School multiplication took "
+              << chrono::duration_cast<chrono::milliseconds>(t2-t1).count()
+              << " milliseconds to return " << result << "\n";
 
     return 0;
 }
